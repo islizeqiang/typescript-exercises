053610517ce82d1527b3bcedd719b99ff027bdbf
islizeqiang <islizeqiang@gmail.com>
2020-05-12 23:49:18 +0800

    feat: 解决

--- exercises/exercise-02/index.ts
+++ exercises/exercise-02/index.ts
@@ -27,53 +27,73 @@ Run:
 */
 
 interface User {
-    name: string;
-    age: number;
-    occupation: string;
+  name: string;
+  age: number;
+  occupation: string;
 }
 
 interface Admin {
-    name: string;
-    age: number;
-    role: string;
+  name: string;
+  age: number;
+  role: string;
 }
 
 type Person = User | Admin;
 
 const persons: Person[] = [
-    {
-        name: 'Max Mustermann',
-        age: 25,
-        occupation: 'Chimney sweep'
-    },
-    {
-        name: 'Jane Doe',
-        age: 32,
-        role: 'Administrator'
-    },
-    {
-        name: 'Kate Müller',
-        age: 23,
-        occupation: 'Astronaut'
-    },
-    {
-        name: 'Bruce Willis',
-        age: 64,
-        role: 'World saver'
-    }
+  {
+    name: 'Max Mustermann',
+    age: 25,
+    occupation: 'Chimney sweep',
+  },
+  {
+    name: 'Jane Doe',
+    age: 32,
+    role: 'Administrator',
+  },
+  {
+    name: 'Kate Müller',
+    age: 23,
+    occupation: 'Astronaut',
+  },
+  {
+    name: 'Bruce Willis',
+    age: 64,
+    role: 'World saver',
+  },
 ];
 
+// in 操作符
 function logPerson(person: Person) {
-    let additionalInformation: string;
-    if (person.role) {
-        additionalInformation = person.role;
-    } else {
-        additionalInformation = person.occupation;
-    }
-    console.log(` - ${chalk.green(person.name)}, ${person.age}, ${additionalInformation}`);
+  let additionalInformation: string;
+  if ('role' in person) {
+    additionalInformation = person.role;
+  } else {
+    additionalInformation = person.occupation;
+  }
+  console.log(
+    ` - ${chalk.green(person.name)}, ${
+      person.age
+    }, ${additionalInformation}`
+  );
 }
 
-persons.forEach(logPerson);
+// 类型断言
+function logPerson1(person: Person) {
+  let additionalInformation: string;
+  if ((person as Admin).role) {
+    additionalInformation = (person as Admin).role;
+  } else {
+    additionalInformation = (person as User).occupation;
+  }
+  console.log(
+    ` - ${chalk.green(person.name)}, ${
+      person.age
+    }, ${additionalInformation}`
+  );
+}
+
+persons.forEach(logPerson1);
 
 // In case if you are stuck:
 // https://www.typescriptlang.org/docs/handbook/advanced-types.html#using-the-in-operator
